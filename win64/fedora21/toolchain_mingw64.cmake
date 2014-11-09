6ce96c21269c4221735201b83743243ea732cade
napodan <david.guenet@gmail.com>
2014-11-09 13:29:46 +0100

    Add licences header and remove an unused cmake file

--- win64/fedora21/toolchain_mingw64.cmake
+++ win64/fedora21/toolchain_mingw64.cmake
@@ -1,3 +1,21 @@
+###############################################################################
+# Minetest
+# Copyright (C) 2010-2014 celeron55, Perttu Ahola <celeron55@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+###############################################################################
 include( "$ENV{dir}/fedora20/toolchain_mingw64.cmake" )
 
 set( CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS} ${Mingw_Path}/libwinpthread-1.dll)
