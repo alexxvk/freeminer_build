851e3d36079507069c249cd2471d7060f463a8b6
napodan <david.guenet@gmail.com>
2014-11-11 14:33:25 +0100

    Add a TODO

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -17,6 +17,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ###############################################################################
 
+# TODO use the irrlicht one
 find_package(ZLIB)
 if(ZLIB_FOUND)
 	set(ZLIB_INCLUDE_DIR "${ZLIB_INCLUDE_DIR}"
